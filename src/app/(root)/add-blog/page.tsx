1d058ce3d3bc735e03964b0fa56f1ab7dabf7336
RitikaShrestha-me <shrestharitika189@gmail.com>
2025-06-08 16:09:39 +0545

    Added passcode before add-blog

--- src/app/(root)/add-blog/page.tsx
+++ src/app/(root)/add-blog/page.tsx
@@ -14,7 +14,8 @@ export default function AddBlog() {
 	const [type, setType] = useState<string>('');
 	const [imageUrl, setImageUrl] = useState<string>('');
 	const [isUploading, setIsUploading] = useState<boolean>(false);
-
+	const [isVerified, setIsVerified] = useState(false);
+	const [passcode, setPasscode] = useState('');
 
 	const handleSubmit = async () => {
 		if (!title || !content || !imageUrl || !type) {
@@ -46,43 +47,68 @@ export default function AddBlog() {
 		}
 	};
 
+	const verifyPasscode = () => {
+		if (passcode === process.env.NEXT_PUBLIC_PASSCODE) {
+			setIsVerified(true)
+		}
+	}
+
 	return (
-		<div className="max-w-xl mx-auto bg-white rounded-2xl shadow-md p-8 mt-28 mb-8 space-y-6">
-			<h1 className="text-2xl font-bold text-gray-900">Add a Blog Post</h1>
+		<>
+			{!isVerified ?
+				<div className="max-w-xl mx-auto bg-white rounded-2xl shadow-md p-8 mt-32 mb-8 space-y-6">
+					<input
+						type="text"
+						placeholder="Enter Passcode"
+						value={passcode}
+						onChange={(e) => setPasscode(e.target.value)}
+						className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+					/>
 
-			<input
-				type="text"
-				placeholder="Title"
-				value={title}
-				onChange={(e) => setTitle(e.target.value)}
-				className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-			/>
+					<button
+						onClick={verifyPasscode}
+						className="w-full py-3 mt-4 text-white bg-blue-600 hover:bg-blue-700 rounded-lg font-semibold transition duration-200 disabled:opacity-50"
+					>
+						Submit Passcode
+					</button>
+				</div>
+				:
+				<div className="max-w-xl mx-auto bg-white rounded-2xl shadow-md p-8 mt-28 mb-8 space-y-6">
+					<h1 className="text-2xl font-bold text-gray-900">Add a Blog Post</h1>
 
-			<input
-				type="text"
-				placeholder="Type"
-				value={type}
-				onChange={(e) => setType(e.target.value)}
-				className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-			/>
+					<input
+						type="text"
+						placeholder="Title"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+					/>
 
-			<textarea
-				placeholder="Content"
-				value={content}
-				onChange={(e) => setContent(e.target.value)}
-				className="w-full px-4 py-3 border border-gray-300 rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
-				rows={6}
-			/>
+					<input
+						type="text"
+						placeholder="Type"
+						value={type}
+						onChange={(e) => setType(e.target.value)}
+						className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+					/>
 
-			<input
-				type="text"
-				placeholder="Image Url"
-				value={imageUrl}
-				onChange={(e) => setImageUrl(e.target.value)}
-				className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-			/>
+					<textarea
+						placeholder="Content"
+						value={content}
+						onChange={(e) => setContent(e.target.value)}
+						className="w-full px-4 py-3 border border-gray-300 rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+						rows={6}
+					/>
 
-			{/*<div>
+					<input
+						type="text"
+						placeholder="Image Url"
+						value={imageUrl}
+						onChange={(e) => setImageUrl(e.target.value)}
+						className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+					/>
+
+					{/*<div>
 				<label className="block mb-1 text-sm font-medium text-gray-700">Upload Cover Image</label>
 				 <input
 					type="file"
@@ -96,13 +122,15 @@ export default function AddBlog() {
 				/> 
 			</div>*/}
 
-			<button
-				onClick={handleSubmit}
-				disabled={isUploading}
-				className="w-full py-3 mt-4 text-white bg-blue-600 hover:bg-blue-700 rounded-lg font-semibold transition duration-200 disabled:opacity-50"
-			>
-				{isUploading ? 'Uploading...' : 'Submit Blog'}
-			</button>
-		</div>
+					<button
+						onClick={handleSubmit}
+						disabled={isUploading}
+						className="w-full py-3 mt-4 text-white bg-blue-600 hover:bg-blue-700 rounded-lg font-semibold transition duration-200 disabled:opacity-50"
+					>
+						{isUploading ? 'Uploading...' : 'Submit Blog'}
+					</button>
+				</div>
+			}
+		</>
 	);
 }
